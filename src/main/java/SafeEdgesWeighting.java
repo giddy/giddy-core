9686a5487fbf642d8f46c49272c7c1f1c8914519
Robi Kovacs <robi.kovacs11@gmail.com>
2016-08-16 21:30:39 +0300

    Init project

---
+++ src/main/java/SafeEdgesWeighting.java
@@ -0,0 +1,60 @@
+import com.graphhopper.routing.util.AbstractAdjustedWeighting;
+import com.graphhopper.routing.util.FlagEncoder;
+import com.graphhopper.routing.util.HintsMap;
+import com.graphhopper.routing.util.Weighting;
+import com.graphhopper.util.EdgeIteratorState;
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Created by rik on 8/16/16.
+ */
+public class SafeEdgesWeighting extends AbstractAdjustedWeighting
+{
+    // contains the edge IDs of the already visited edges
+    protected final TIntSet visitedEdges = new TIntHashSet();
+
+    private double edgePenaltyFactor = 5.0;
+
+    public SafeEdgesWeighting(FlagEncoder superWeighting)
+    {
+        super(superWeighting);
+    }
+
+    public SafeEdgesWeighting setEdgePenaltyFactor(double edgePenaltyFactor )
+    {
+        this.edgePenaltyFactor = edgePenaltyFactor;
+        return this;
+    }
+
+    /**
+     * This method adds the specified path to this weighting which should be penalized in the
+     * calcWeight method.
+     * @param edges
+     */
+    public void addEdges(Set<Integer> edges )
+    {
+       // TODO
+    }
+
+    @Override
+    public double getMinWeight( double distance )
+    {
+        // TODO
+    }
+
+    @Override
+    public double calcWeight( EdgeIteratorState edgeState, boolean reverse, int prevOrNextEdgeId )
+    {
+        // TODO
+    }
+
+    @Override
+    public String getName()
+    {
+        return "safe_edges";
+    }
+}
