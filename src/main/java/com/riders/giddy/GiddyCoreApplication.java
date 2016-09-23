26ea7c3a8aa55305a3c42497df7884bc005a0bf8
Robi Kovacs <robi.kovacs11@gmail.com>
2016-09-23 22:14:26 +0300

    Fix identation settings

--- src/main/java/com/riders/giddy/GiddyCoreApplication.java
+++ src/main/java/com/riders/giddy/GiddyCoreApplication.java
@@ -6,17 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 /**
- * TODO Add Javadoc comment.
- */
+* TODO Add Javadoc comment.
+*/
 @SpringBootApplication
 @EnableJSONDoc
 public class GiddyCoreApplication {
 
     /**
-     * TODO Add javadoc.
-     * @param args
-     */
-	public static void main(final String[] args) {
-		SpringApplication.run(GiddyCoreApplication.class, args);
-	}
+    * TODO Add javadoc.
+    * @param args Something
+    */
+
+    public static void main(final String[] args) {
+    	SpringApplication.run(GiddyCoreApplication.class, args);
+    }
 }
--- src/main/java/com/riders/giddy/HelloController.java
+++ src/main/java/com/riders/giddy/HelloController.java
@@ -1,7 +1,7 @@
 package com.riders.giddy;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
