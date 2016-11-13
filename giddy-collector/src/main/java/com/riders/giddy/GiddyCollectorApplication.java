73354a6e32e78e03e60f537eff803f8045ef09ac
Robi Kovacs <robi.kovacs11@gmail.com>
2016-11-13 14:44:56 +0200

    Major refactor

---
+++ giddy-collector/src/main/java/com/riders/giddy/GiddyCollectorApplication.java
@@ -0,0 +1,12 @@
+package com.riders.giddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiddyCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiddyCollectorApplication.class, args);
+	}
+}
---
+++ giddy-collector/src/test/java/com/riders/giddy/GiddyCollectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.riders.giddy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiddyCollectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
