9c2206002f87cf11f8b92ecd9d1b83c450fe2322
tudor beleuta <tudor.beleuta@gmail.com>
2017-01-13 01:36:23 +0200

    add properties file on test

---
+++ src/test/java/com/riders/giddy/BaseTest.java
@@ -0,0 +1,14 @@
+package com.riders.giddy;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@EnableAutoConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseTest {
+
+    Map
+}
